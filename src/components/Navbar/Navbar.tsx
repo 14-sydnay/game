59df4efe09580270a9590b20c46011d90e1cf478
kapit0shka <neverlle98@gmail.com>
2022-06-21 23:58:29 +0500

    добавил ссылку на вход

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -29,7 +29,9 @@ export const Navbar: React.FC<{}> = () => {
         </ul>
       </nav>
       <div className="navbar-end">
-        <a className="btn btn-secondary">Войти</a>
+        <Link to="/login" className="btn btn-secondary">
+          Войти
+        </Link>
       </div>
     </div>
   )
