6846c9664bc4beab223c8ce7649633828e005046
Anton Lukyanov <lukyanovanton13@yandex.ru>
2022-07-21 23:26:33 +0600

    save user theme in db

--- src/server/controllers/userTheme.ts
+++ src/server/controllers/userTheme.ts
@@ -1,5 +1,8 @@
-import { UserThemeDto } from 'api/userTheme'
 import { Request, Response, NextFunction } from 'express'
+
+import { UserThemeModel } from '../db/init'
+import { UserThemeDto } from 'api/userTheme'
+
 export const getUserTheme = (
   req: Request,
   res: Response,
@@ -12,12 +15,20 @@ export const getUserTheme = (
     res.status(200).json({ themeName: 'dark', userId: req.params.id })
   }
 }
-export const saveUserTheme = (
+export const saveUserTheme = async (
   req: Request,
   res: Response,
   next: NextFunction
-): void => {
+): Promise<void> => {
   const body = req.body as UserThemeDto
+  const userTheme = await UserThemeModel.findOne({
+    where: { userId: body.userId },
+  })
+  if (userTheme) {
+    await UserThemeModel.update(body, { where: { userId: body.userId } })
+  } else {
+    await UserThemeModel.create(body)
+  }
 
   res.status(200).json({ themeName: body.themeName, userId: req.params.id })
 }
