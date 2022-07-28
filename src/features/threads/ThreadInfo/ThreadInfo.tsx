4d14e616261bd1fa33b62b4ee1e54bc5d8ae14a8
kapitoshka808 <neverlle98@gmail.com>
2022-07-28 21:53:04 +0500

    fix thread info

--- src/features/threads/ThreadInfo/ThreadInfo.tsx
+++ src/features/threads/ThreadInfo/ThreadInfo.tsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs'
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 import { selectThreadById } from '../threadsSlice'
@@ -9,20 +9,25 @@ import { AuthorName } from 'features/authors/AuthorName'
 import { Thread } from 'src/server/models/thread'
 
 export const ThreadInfo: FC<Props> = ({ threadId }) => {
-  const thread = useSelector((state) =>
+  const [thread, setThread] = useState<Thread>()
+  const threadData = useSelector((state) =>
     selectThreadById(state, threadId)
   ) as Thread
 
+  useEffect(() => {
+    setThread(threadData)
+  }, [threadData])
+
   return (
     <div className="mt-10 flex items-center">
-      <AuthorAvatar authorId={thread.authorId} />
+      <AuthorAvatar authorId={thread?.authorId} />
       <div className="ml-4">
-        <h1 className="text-lg font-medium text-gray-900">{thread.title}</h1>
+        <h1 className="text-lg font-medium text-gray-900">{thread?.title}</h1>
         <div className="flex flex-row">
-          <AuthorName authorId={thread.authorId} />
+          <AuthorName authorId={thread?.authorId} />
           <span className="divider divider-horizontal mx-1" />
           <span className="text-md text-gray-500">
-            {dayjs(thread.created).format('DD.MM.YYYY HH:mm')}
+            {dayjs(thread?.created).format('DD.MM.YYYY HH:mm')}
           </span>
         </div>
       </div>
